ae356f69a7fb2b47ecaf0c882ab06c4d91db39d9
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-02-21 17:37:37 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react"
 import Board from "./components/Board.jsx"
 import "./styles/App.scss"
 
 function App() {
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem("theme") || "dark"
+    })
+    useEffect(() => {
+        document.body.className = theme
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
+    function toggleTheme() {
+        setTheme((prev) => (prev === "dark" ? "light" : "dark"))
+    }
     return (
         <div>
-            <h1>Dashboard</h1>
+            <header>
+                <h1>Dashboard</h1>
+                <button onClick={toggleTheme}>
+                    {" "}
+                    {theme === "dark" ? "☀️ Claro" : "🌙 Escuro"}
+                </button>
+            </header>
             <Board />
         </div>
     )
